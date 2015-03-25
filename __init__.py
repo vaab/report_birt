749b6e2b5aff74a1abfb50ca21e412c75143b79e
Valentin Lab <valentin.lab@kalysto.org>
2015-03-25 16:16:53 +0800

    fix: v8 wouldn't see the report type ``BIRT`` (fixes #6)

--- __init__.py
+++ __init__.py
@@ -4,3 +4,12 @@ import birt_report
 import birt_report_parameter
 import ir_report
 
+
+## Monkey patching to add a new type of reports:
+
+from openerp.addons.base.ir.ir_actions import ir_actions_report_xml
+
+column = ir_actions_report_xml._columns['report_type']
+if getattr(column, '_type', False) == 'selection':
+    ## then it's a v8
+    column.selection.append(('birt', 'BIRT report'))
